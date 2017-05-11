f8af08fe125e3aba1a2b7b3f63690c91cf657fe4
Anton Glushkov <alexandrovich.anton7@gmail.com>
2017-05-11 17:17:38 +0400

    added input values validation

--- src/main/resources/static/js/decision.js
+++ src/main/resources/static/js/decision.js
@@ -1,11 +1,13 @@
 $(function () {
+    //TODO show start page
+
     var countOfInputs = 1;
     $('#addInputParameter').on('click', function (event) {
         event.preventDefault();
         var className = 'paramItem' + countOfInputs;
 
         var block = $("<div class='" + className + "'></div>");
-        var input = $("<div class='col-md-11'><input type='number' class='form-control inParam' id='id" + countOfInputs + "'/></div>");
+        var input = $("<div class='col-md-11'><input type='number' required='required' class='form-control inParam' id='id" + countOfInputs + "'/></div>");
         var btnBlock = $("<div class='col-md-1'></div>");
         var btn = $("<button type='button' value='" + countOfInputs + "' class='btn btn-danger inParam removeParamBtn' id='deleteParameter" + countOfInputs + "'><span class='glyphicon glyphicon-remove' aria-hidden='true'></span></button>");
 
@@ -23,8 +25,45 @@ $(function () {
         countOfInputs += 1;
     });
 
-    function removeItemById(id) {
-        var className = ".paramItem" + id;
-        $(className).remove();
-    }
+    $('#chooseModel').on('click', function (event) {
+        event.preventDefault();
+
+        if (!validate()) {
+            return;
+        }
+
+        $("#inputsErrorBlock").hide();
+
+        $('#input-values-module').hide();
+        $('#choose-model-module').show();
+    });
+
+    $('#backToInputValuesBtn').on('click', function (event) {
+        event.preventDefault();
+
+        $('#input-values-module').show();
+        $('#choose-model-module').hide();
+    });
 });
+
+function validate() {
+    var msg = $("#inputsErrorMessage");
+    var errorBlock = $("#inputsErrorBlock");
+
+    var flag = true;
+
+    $('.inParam').each(function (i, item) {
+        var value = item.value;
+        if (value === "") {
+            msg.html("Заполните все входные значения");
+            errorBlock.show();
+            flag = false;
+        }
+    });
+    return flag;
+}
+
+function removeItemById(id) {
+    var className = ".paramItem" + id;
+    $(className).remove();
+}
